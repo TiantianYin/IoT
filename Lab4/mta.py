f41c07eab8ea4f2ca4b63b31c82f1dd094fe36e7
Hongyu <hy2504@columbia.edu>
2017-02-26 21:09:45 -0500

    start

--- Lab4/mta.py
+++ Lab4/mta.py
@@ -12,16 +12,49 @@ import aws
 
 DYNAMODB_TABLE_NAME = "mtaData"
 
+dynamodb = aws.getResource('dynamodb', 'us-east-1')
+
+table = dynamodb.Table(DYNAMODB_TABLE_NAME)
+
+
 # prompt
 def prompt():
-    print ""
-    print ">Available Commands are : "
-    print "1. plan trip"
-    print "2. subscribe to messages"
-    print "3. exit"  
+	print ""
+	print ">Available Commands are : "
+	print "1. plan trip"
+	print "2. subscribe to messages"
+	print "3. exit"  
+
+tripID1 = '103100_4..N34R'
+routeId1 = '4'
 
 def main():
+	prompt()
+	response = table.scan(
+		#KeyConditionExpression=Key('year').eq(1985)
+		FilterExpression = Key('startDate').between('20170220', '20170222')
+		#FilterExpression = Key('startDate').eq(20170221)
+	)
+	result=table.delete_item( 
+		Key={
+			'tripId': tripID1,
+			'routeId': routeId1
+		}
+	)
+
+
+  	for i in response['Items']:
+		result=table.delete_item( 
+			Key={
+				'tripId': i['tripId'],
+				'routeId': i['routeId']
+			}
+		)
+
 	return 0
+
+
+
 
 
 if __name__ == "__main__":
