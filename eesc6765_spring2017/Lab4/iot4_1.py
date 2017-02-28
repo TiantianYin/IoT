b8447c846add376e52f381f478b83612489ec52e
TiantianYin <ty2346@columbia.edu>
2017-02-27 21:08:59 -0500

    4_1

--- eesc6765_spring2017/Lab4/iot4_1.py
+++ eesc6765_spring2017/Lab4/iot4_1.py
@@ -5,13 +5,6 @@ import mraa
 import pyupm_i2clcd as lcd
 import math
 
-switch_pin_number=8
- 
-# Configuring the switch and buzzer as GPIO interfaces
-switch = mraa.Gpio(switch_pin_number)
-
-switch.dir(mraa.DIR_IN)
-
 tempSensor = mraa.Aio(1)
 temp = float (tempSensor.read())
 temp = 1023.0/(temp)-1.0;
@@ -23,13 +16,6 @@ client = boto3.client('sns', 'us-east-1')
 msg = "The current temprature is: " + str(temp)
 
 print msg
-"""
-response = client.publish(
-    TopicArn='arn:aws:sns:us-east-1:768104751743:IoT_Lab4_1',
-    #TargetArn='arn:aws:sns:us-east-1:768104751743:IoT_Lab4_1:1a93e108-c975-437e-bd2e-ef08a6bc69d9',
-    Message='aaaaa'
-)
-"""
 
 logging.basicConfig(filename="sns-publish.log", level=logging.DEBUG)
 
@@ -40,5 +26,3 @@ message = msg
 message_subject = "trialBotoTRopic"
 
 publication = c.publish(topicarn, message, subject=message_subject)
-
-print publication
