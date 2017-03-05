d453ea15d7226551f2be4a52760fa2314af00730
Kevin Xing <uestcxzk@gmail.com>
2017-03-05 16:21:17 -0500

    lab5

---
+++ eesc6765_spring2017/Lab5/dynamodata.py
@@ -0,0 +1,127 @@
+# *********************************************************************************************
+# Program to update dynamodb with latest data from mta feed. It also cleans up stale entried from db
+# Usage python dynamodata.py
+# *********************************************************************************************
+#from __future__ import print_function # Python 2/3 compatibility
+#import boto3
+import json,time,sys
+import threading
+from collections import OrderedDict
+from threading import Thread
+
+import time
+
+import boto3
+from boto3.dynamodb.conditions import Key,Attr
+#from __future__ import print_function # Python 2/3 compatibility
+#import boto3
+
+sys.path.append('../utils')
+import tripupdate,vehicle,alert,mtaUpdates,aws
+
+### YOUR CODE HERE ####
+
+dynamodb = aws.getResource('dynamodb', 'us-east-1')
+
+
+try:
+
+  table = dynamodb.create_table(
+    TableName='Lab5_new',
+    KeySchema=[
+        {
+            'AttributeName': 'tripId',
+            'KeyType': 'HASH'  #Partition key
+        },
+        {
+            'AttributeName': 'routeId',
+            'KeyType': 'RANGE'  #Sort key
+        }
+    ],
+
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'tripId',
+            'AttributeType': 'S'
+        },
+		    {
+            'AttributeName': 'routeId',
+            'AttributeType': 'S'
+        },
+
+    ],
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 10,
+        'WriteCapacityUnits': 10
+    }
+  )
+  print("Table status:", table.table_status)
+except:
+  table = dynamodb.Table('Lab5_new')
+        #add function
+
+#=====
+#threads
+
+#import threading
+
+with open('key.txt', 'rb') as keyfile:
+        APIKEY = keyfile.read().rstrip('\n')
+        keyfile.close()
+#newUpdate = mtaUpdates.mtaUpdates()
+
+def add():
+  print 'Adding!'
+  newUpdate = mtaUpdates.mtaUpdates(APIKEY)
+  tr = newUpdate.getTripUpdates()
+  for entity in tr:
+   	response = table.put_item(
+     		Item={
+          	'tripId': entity[0],
+          	'routeId': entity[1],
+          	'timestamp':entity[2],
+            'dayOfWeek':entity[3],
+            'timeAt96':entity[4],
+            'timeAt42':entity[5]
+      	     }
+    	)
+  time.sleep(30)
+
+def task2():
+  time.sleep(20)
+  print("Deleting!")
+  min_time = int(time.time()) - 120
+  response = table.scan(
+    FilterExpression = Key('timeStamp').between(0, min_time)
+  )
+
+  for i in response['Items']:
+    result=table.delete_item(
+      Key={
+        'tripId': i['tripId'],
+        'routeId': i['routeId']
+      }
+    )
+  time.sleep(40)
+
+
+if __name__ == '__main__':
+  threads = []
+  thread1 = Thread(target = add)
+  thread1.setDaemon(True)
+  #thread2 = Thread(target = task2)
+  #thread2.setDaemon(True)
+  thread1.start()
+  #thread2.start()
+  threads.append(thread1)
+  #threads.append(thread2)
+
+  try:
+    while(True):
+      time.sleep(0.5)
+  except KeyboardInterrupt:
+    for t in threads:
+      t.join()
+    exit()
+    print "Exit! But Daemon Remains!"
+
---
+++ eesc6765_spring2017/utils/mtaUpdates5.py
@@ -0,0 +1,85 @@
+import urllib2,contextlib
+from datetime import datetime
+from collections import OrderedDict
+
+from pytz import timezone
+import gtfs_realtime_pb2
+import google.protobuf
+import copy
+import vehicle,alert,tripupdate
+
+import urllib,urllib2
+import sys
+
+class mtaUpdates5(object):
+
+    # Do not change Timezone
+    TIMEZONE = timezone('America/New_York')
+    
+    # feed url depends on the routes to which you want updates
+    # here we are using feed 1 , which has lines 1,2,3,4,5,6,S
+    # While initializing we can read the API Key and add it to the url
+    feedurl = 'http://datamine.mta.info/mta_esi.php?feed_id=1&key='
+    
+    VCS = {1:"INCOMING_AT", 2:"STOPPED_AT", 3:"IN_TRANSIT_TO"}    
+    tripUpdates = []
+    alerts = []
+
+    def __init__(self,apikey):
+        self.feedurl = self.feedurl + apikey
+
+    # Method to get trip updates from mta real time feed
+    def getTripUpdates(self):
+        feed = gtfs_realtime_pb2.FeedMessage()
+        try:
+            with contextlib.closing(urllib2.urlopen(self.feedurl)) as response:
+                d = feed.ParseFromString(response.read())
+        except (urllib2.URLError, google.protobuf.message.DecodeError) as e:
+            print "Error while connecting to mta server " +str(e)
+    
+        timestamp = feed.header.timestamp
+        start_time = datetime.datetime.combine(datetime.date.today(), datetime.time.min)
+        timestamp = (timestamp - start_time).total_seconds() / 60
+        dayOfWeek = datetime.datetime.today().weekday()
+        if (int(dayOfWeek) >= 5):
+            dayOfWeek = weekend
+        else:
+            dayOfWeek = weekday
+        nytime = datetime.fromtimestamp(timestamp,self.TIMEZONE)
+
+
+        for entity in feed.entity:
+        # Trip update represents a change in timetable
+            if entity.HasField('trip_update'):
+                tmp = []
+                #self.tripUpdates.append([])
+                update = tripupdate.tripupdate()
+                tmp.append(entity.trip_update.trip.trip_id)
+                tmp.append(entity.trip_update.trip.route_id)
+                tmp.append(timestamp)
+                tmp.append(dayOfWeek)
+                tmp.append(-1)
+                tmp.append(-1)
+                # find 96th and 42th street
+                find = 0
+                for stop in entity.trip_update.stop_time_update:
+                    if (stop.stop_id == '120S' or stop.stop_id == '120N'):
+                        find = find + 1
+                        tmp[4] = (stop.arrival.time)
+                    if (stop.stop_id == '631S' or stop.stop_id == '631N'):
+                        find = find + 1
+                        tmp[5] = (stop.arrival.time)
+                if (find < 2):
+                    continue
+                self.tripUpdates.append(tmp)
+
+            if entity.HasField('vehicle'):
+                if (entity.vehicle.stop_id == '631S' or entity.vehicle.stop_id == '631N'):
+                    self.tripUpdates[len(self.tripUpdates)-1][5] = entity.vehicle.timestamp
+
+            #### INSERT ALERT CODE HERE #####
+
+        
+        return self.tripUpdates
+    
+
